cb282b4d7f4f691164be14d6f7f820d37406f2be
Jeferson Camargo <j.camargo@superpaos.com>
2022-05-18 07:59:15 -0500

    First challenge solution

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <InputAutocorrect></InputAutocorrect>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputAutocorrect from "@/components/InputAutocorrect";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InputAutocorrect,
   }
 }
 </script>
---
+++ src/components/InputAutocorrect.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <input v-model="word"
+         placeholder="Write a word">
+
+</template>
+
+<script>
+export default {
+  name: "InputAutocorrect",
+  data() {
+    return {
+      word: '',
+      words: {
+        'correjir:': 'corregir',
+        'correcion': 'corrección',
+        'reevaluar': 'revaluar',
+        'tezt': 'test',
+      }
+    }
+  },
+  watch: {
+    word(value, oldValue) {
+      if (value in this.words) {
+        this.word = this.words[value]
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
